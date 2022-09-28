8c299be3bfb56877b6aaed266cec995a0138c1b8
tacticoolshik <103657820+tacticoolshik@users.noreply.github.com>
2022-09-28 08:21:17 +0300

    Update rps-game.py

--- rps-game.py
+++ rps-game.py
@@ -1,17 +1,32 @@
-human_turn = input("Input human turn: ")
-computer_turn = input("Input computer turn: ")
+import random
 
-if human_turn == computer_turn:
-    print("Draw")
+def rps():
+    turns = ["rock", "paper", "scissors"]
 
-if human_turn == "rock" and computer_turn == "scissors":
-    print("Human wins")
+    human_turn = input("Input human turn: ")
 
-elif human_turn == "paper" and computer_turn == "rock":
-    print("Human wins")
+    computer_turn = random.choice(turns)
 
-elif human_turn == "scissors" and computer_turn == "paper":
-    print("Human wins")
+    print(f"Human: {human_turn} vs Computer: {computer_turn}")
 
-else:
-    print("Computer Wins")
+    if human_turn == computer_turn:
+        print("Draw")
+
+    if human_turn == "rock" and computer_turn == "scissors":
+        print("Human wins")
+
+    elif human_turn == "paper" and computer_turn == "rock":
+        print("Human wins")
+
+    elif human_turn == "scissors" and computer_turn == "paper":
+        print("Human wins")
+
+    else:
+        print("Computer Wins")
+    
+    restart = input("Wanna play again? Write y or n: ")
+    if restart == "y":
+        rps()
+    else:
+        exit
+rps()
